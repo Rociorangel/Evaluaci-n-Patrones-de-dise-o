b77d479dc9430ad643802b710ceb5714579ae2e1
Rociorangel <56567878+Rociorangel@users.noreply.github.com>
2019-11-25 19:19:57 -0600

    Update Template.py

--- Template/Template.py
+++ Template/Template.py
@@ -3,11 +3,13 @@ from abc import ABC, abstractmethod
 class Dispositivo(ABC):
     @abstractmethod
     def __init__(self):
-        self._cantidadDipositivos= Dispositivo()
+        self._cantidadDipositivos= None
     
 class Celular(Dispositivo):
     def tipoDispositivo(self):
          print("Tiene un tamaño de 10 cm")
+    def Precio(self):
+         print("Tiene un tamaño de 10 mil pesos")
 
 class Tablet (Dispositivo):
     def tipoDispositivo(self):
